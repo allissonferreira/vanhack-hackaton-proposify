de94ebf465c9c463d043a4ba2754292c57acb121
Allisson Ferreira <allisson.ferreira@outlook.com>
2019-03-23 15:02:13 -0300

    Adding comments

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-﻿// Modules
-var express = require('express');
-	app = express(),
-	http = require('http').Server(app),
-	io = require('socket.io')(http),
-	realtimeEditor = require('realtime-editor');	
+﻿//Modules
+var express 		= require('express');
+	app 			= express(),
+	http 			= require('http').Server(app),
+	io 				= require('socket.io')(http),
+	realtimeEditor 	= require('realtime-editor');	
 
+//Folders used in index.html
 app.use(express.static('public'));
 app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
-// App routing
+//App routing
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-// realtimeEditor hook for saving content
-// the data object have several properties including a custom property object which can hold your app specific IDs
+//realtimeEditor hook for saving content
+//the data object have several properties including a custom property object which can hold your app specific IDs
 realtimeEditor.onSave(function (data) {
 	console.log('realtimeEditor.onSave: ', data);
 });
 
-
+//Start the server
 http.listen(2000, function () {
 	console.log('listening on *:2000');
 });
