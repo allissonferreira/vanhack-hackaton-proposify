af6437ab3cc042c76a14bdbc99b32aa4261fffb3
Allisson Ferreira <allisson.ferreira@outlook.com>
2019-03-23 14:10:39 -0300

    MVP

--- README.md
+++
@@ -1,4 +0,0 @@
-# Proposify Hackaton - Vanhack fair
-
-Hackaton project from scratch to be submitted to Proposify.
-This project is aiming to add some features to Proposify's product.
---
+++ index.js
@@ -0,0 +1,25 @@
+﻿// Modules
+var express = require('express');
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http),
+	realtimeEditor = require('realtime-editor');	
+
+app.use(express.static('public'));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
+
+// App routing
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+// realtimeEditor hook for saving content
+// the data object have several properties including a custom property object which can hold your app specific IDs
+realtimeEditor.onSave(function (data) {
+	console.log('realtimeEditor.onSave: ', data);
+});
+
+
+http.listen(2000, function () {
+	console.log('listening on *:2000');
+});
